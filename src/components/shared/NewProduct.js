874eb21c6c4ff7538d9a53b4bcd3bc2c75233bd4
Zahra Shokri <zvahedi5@gmail.com>
2023-05-07 19:25:12 +0430

    New Product Component is created for show 1 product

---
+++ src/components/shared/NewProduct.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { shorten } from '../../functions/shorten';
+import styles from '../../styles/NewProduct.module.css';
+
+const NewProduct = ({ data }) => {
+    return(
+        <div className={styles.container}>
+            <img src={data.image} alt='products' />
+            <h3> {shorten(data.title)} </h3>
+            <p> {data.price} $ </p>
+
+            <div className={styles.btnsLink}>
+                <Link to={ `/products/${data.id}` }>Details</Link>
+                <button> Add to Cart </button>
+            </div>
+        </div>
+    );
+}
+
+export default NewProduct;
