5aca1e6e0913943c27560b88a4af7a784413cb14
Zahra Shokri <zvahedi5@gmail.com>
2023-05-07 17:08:50 +0430

    Footer Component is added to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
-import Header from "./components/Header";
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import './App.css';
 
 const App = () => {
   return (
     <div>
       <Header />
+      
+      <Footer />
     </div> 
   );
 }
