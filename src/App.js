25302d543e8accfade174df6e283c8afac747125
Zahra Shokri <zvahedi5@gmail.com>
2023-05-07 19:30:22 +0430

    - New Products Component is added to App instead of "Products" Component. - Products Context Component is added to App to use products's data.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from './components/Header';
 import HomePage from './components/HomePage';
-import Products from './components/Products';
+// import Products from './components/Products';
+import NewProducts from './components/NewProducts';
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Footer from './components/Footer';
 import NotFound from './components/NotFound';
+import ProductsContext from './context/ProductsContext';
 
 import './App.css';
 
@@ -18,16 +20,19 @@ const App = () => {
     <div>
       <Header />
 
-      <Switch>
-        <Route path='/products' component={Products} />
-        <Route exact path='/aboutus' component={AboutUs}/>
-        <Route path='/contactus' render={(props) => <ContactUs firstName='Zahra' lastName='Shokri' {...props} />} />
-        <Route path='/signup' component={SignUp} />
-        <Route path='/login' component={Login} />
-        <Route path='/notfound' component={NotFound} />
-        <Route exact path='/' component={HomePage} />
-        <Redirect to='/notfound' from='/*' /> 
-      </Switch>
+      <ProductsContext>
+        <Switch>
+          {/* <Route path='/products' component={Products} /> */}
+          <Route path='/products' component={NewProducts} />
+          <Route exact path='/aboutus' component={AboutUs}/>
+          <Route path='/contactus' render={(props) => <ContactUs firstName='Zahra' lastName='Shokri' {...props} />} />
+          <Route path='/signup' component={SignUp} />
+          <Route path='/login' component={Login} />
+          <Route path='/notfound' component={NotFound} />
+          <Route exact path='/' component={HomePage} />
+          <Redirect to='/notfound' from='/*' /> 
+        </Switch>
+      </ProductsContext>
 
       <Footer />
     </div> 
