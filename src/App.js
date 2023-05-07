1a0aeae984379ad316e21eab220a479aac547fb6
Zahra Shokri <zvahedi5@gmail.com>
2023-05-07 17:48:16 +0430

    NotFound Component is added to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from './components/Header';
 import HomePage from './components/HomePage';
@@ -9,6 +9,7 @@ import ContactUs from './components/ContactUs';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 
 import './App.css';
 
@@ -23,7 +24,9 @@ const App = () => {
         <Route path='/contactus' render={(props) => <ContactUs firstName='Zahra' lastName='Shokri' {...props} />} />
         <Route path='/signup' component={SignUp} />
         <Route path='/login' component={Login} />
+        <Route path='/notfound' component={NotFound} />
         <Route exact path='/' component={HomePage} />
+        <Redirect to='/notfound' from='/*' /> 
       </Switch>
 
       <Footer />
