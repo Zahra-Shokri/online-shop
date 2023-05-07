846b582128134e8b14ab91e33f6d094e64d8cbb3
Zahra Shokri <zvahedi5@gmail.com>
2023-05-07 17:19:30 +0430

    About Us Component is created

---
+++ src/components/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutUs = () => {
+    return (
+        <div style={{minHeight: '100vh'}}>
+            <h2> AboutUs </h2>
+        </div>
+    );
+}
+
+export default AboutUs;
