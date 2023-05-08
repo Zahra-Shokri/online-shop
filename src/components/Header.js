7f0464d9ea131a36afa9f308da9b694a71d5202c
Zahra Shokri <zvahedi5@gmail.com>
2023-05-08 18:41:14 +0430

    a few changes (No matter)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import userProfile from '../images/icon/user.png';
 
-const Div = styled.div`
-    display: flex;
-    padding: 10px;
-    align-items: center;
-    justify-content: space-between;
-    background-color: rgb(190, 190, 190);
+// const Div = styled.div`
+//     display: flex;
+//     padding: 10px;
+//     align-items: center;
+//     justify-content: space-between;
+//     background-color: rgb(190, 190, 190);
 
-    img {
-        width: 45px;
-        margin: 0 20px;
-        border-radius: 100%;
-        background-color: rgb(214, 214, 214);
-    }
+//     img {
+//         width: 45px;
+//         margin: 0 20px;
+//         border-radius: 100%;
+//         background-color: rgb(214, 214, 214);
+//     }
 
-    img:hover {
-        cursor: pointer;
-    }
-`;
+//     img:hover {
+//         cursor: pointer;
+//     }
+// `;
 
-const Ul = styled.ul`
-    display: flex;
-    list-style: none;
-    
-    justify-content: space-around;
+// const Ul = styled.ul`
+//     display: flex;
+//     list-style: none;
+//     justify-content: space-around;
 
-    a {
-        margin: 0 30px;
-        color: #4d4d4d;
-        text-decoration: none;
-    }
-`;
+//     a {
+//         margin: 0 30px;
+//         color: #4d4d4d;
+//         text-decoration: none;
+//     }
+// `;
 
 const Header = () => {
     return(
-        <Div>
-            <Ul>
-                <li> <Link to='/'> Home Page </Link> </li>
-                <li> <Link to='/products'> Products </Link> </li>
-                <li> <Link to='/aboutus'> About Us </Link> </li>
-                <li> <Link to='/contactus'> ContactUs </Link> </li>
-            </Ul>
+        <div style={{display: 'flex', padding: '10px', alignItems: 'center', justifyContent: 'space-between', backgroundColor: 'rgb(190, 190, 190)'}}>
+            <ul style={{display: 'flex', listStyle: 'none', justifyContent: 'space-around'}}>
+                <li> <Link to='/' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> Home Page </Link> </li>
+                <li> <Link to='/products' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> Products </Link> </li>
+                <li> <Link to='/aboutus' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> About Us </Link> </li>
+                <li> <Link to='/contactus' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> ContactUs </Link> </li>
+            </ul>
 
-            <img src={userProfile} alt='User Profile' />
-        </Div>
+            <img src={userProfile} alt='User Profile' style={{width: '45px', margin: '0 20px', borderRadius: '100%', backgroundColor: 'rgb(214, 214, 214)'}} />
+        </div>
     );
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Div = styled.div`
-color: white;
-    display: flex;
-    transition: 1s;
-    margin: 25px 0;
-    align-item: center;
-    padding: 75px 55px;
-    background-color: #30c0e4;
-    justify-content: space-between;
+// const Div = styled.div`
+// color: white;
+//     display: flex;
+//     transition: 1s;
+//     margin: 25px 0;
+//     align-item: center;
+//     padding: 75px 55px;
+//     background-color: #30c0e4;
+//     justify-content: space-between;
     
-    &:hover {
-        background-color: #b5b5b5;
-    }
-    `;
+//     &:hover {
+//         background-color: #b5b5b5;
+//     }
+// `;
     
 class Search extends Component {
     constructor() {
@@ -63,7 +63,7 @@ class Search extends Component {
         const inputStyle = {width: '350px', height: '40px', fontSize:'15px', padding: '0 15px', borderRadius: '10px'};
 
         return(
-            <Div>
+            <div style={{display: 'flex', transition: '1s', margin: '25px 0', alignItem: 'center', padding: '75px 55px', backgroundColor: '#30c0e4', justifyContent: 'space-between'}}>
                 <p style={pStyle}>Search What You Want?</p>
                 <input type='text' value={this.state.textVal} onChange={this.changeTextVal} style={inputStyle} placeholder='Search...' />
                 
@@ -85,7 +85,7 @@ class Search extends Component {
 
                 {/* 2-2 Create Error For Show Error Boundry How to Work? */}
                 {/* <button onClick={this.buildErr}>Press Me!</button> */}
-            </Div>
+            </div>
         );
     }
 }
