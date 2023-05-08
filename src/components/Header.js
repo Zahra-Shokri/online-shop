3c07ad567d2713243cd026407b35bcd0251e0697
Zahra Shokri <zvahedi5@gmail.com>
2023-05-08 19:21:45 +0430

    Add shopping cart icon to Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import styled from 'styled-components';
 import userProfile from '../images/icon/user.png';
+import shoppingCart from '../images/icon/shoppingCart.png';
 
 // const Div = styled.div`
 //     display: flex;
@@ -36,15 +37,16 @@ import userProfile from '../images/icon/user.png';
 
 const Header = () => {
     return(
-        <div style={{display: 'flex', padding: '10px', alignItems: 'center', justifyContent: 'space-between', backgroundColor: 'rgb(190, 190, 190)'}}>
-            <ul style={{display: 'flex', listStyle: 'none', justifyContent: 'space-around'}}>
+        <div style={{display: 'flex', padding: '10px', alignItems: 'center', backgroundColor: 'rgb(190, 190, 190)'}}>
+            <ul style={{display: 'flex', listStyle: 'none'}}>
                 <li> <Link to='/' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> Home Page </Link> </li>
                 <li> <Link to='/products' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> Products </Link> </li>
                 <li> <Link to='/aboutus' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> About Us </Link> </li>
                 <li> <Link to='/contactus' style={{margin: '0 30px', color: '#4d4d4d', textDecoration: 'none'}}> ContactUs </Link> </li>
             </ul>
 
-            <img src={userProfile} alt='User Profile' style={{width: '45px', margin: '0 20px', borderRadius: '100%', backgroundColor: 'rgb(214, 214, 214)'}} />
+            <img src={shoppingCart} alt='User Profile' style={{width: '20px', marginLeft: '60%'}} />
+            <img src={userProfile} alt='User Profile' style={{width: '40px', margin: '0 30px', borderRadius: '100%', backgroundColor: 'rgb(214, 214, 214)'}} />
         </div>
     );
 }
