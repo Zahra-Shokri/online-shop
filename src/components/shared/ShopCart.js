e81cdbd51da281498edc5d09f92dd70b35a2e291
Zahra Shokri <zvahedi5@gmail.com>
2023-05-09 20:50:15 +0430

    Shop Cart Component is created with CSS to show 1 product that added to list

---
+++ src/components/shared/ShopCart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Style
+import styles from '../styles/ShopCart.module.css';
+
+const ShopCart = () => {
+    return(
+        <div className={styles.container}>
+            <h1> shop cart list </h1>
+        </div>
+    );
+}
+
+export default ShopCart;
