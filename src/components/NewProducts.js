86371766106b52fcbdaa3049a06a35c043a39a9b
Zahra Shokri <zvahedi5@gmail.com>
2023-05-09 20:35:46 +0430

    A few Changes (Not Matter)

--- src/components/NewProducts.js
+++ src/components/NewProducts.js
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
-import Product from './shared/Product';
-import styles from '../styles/Products.module.css';
+import NewProduct from './shared/NewProduct';
+import styles from '../styles/NewProducts.module.css';
 
 import { ProductsContextProvider } from '../context/ProductsContext';
 
-const Products = () => {
+const NewProducts = () => {
     const productsData = useContext(ProductsContextProvider);
     
     return(
         <div className={styles.container}>
-            { productsData.map(product => <Product key={product.id} data={product} />) }
+            { productsData.map(product => <NewProduct key={product.id} data={product} />) }
         </div>
     );
 }
 
-export default Products;
+export default NewProducts;
