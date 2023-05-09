58f0b4a44e9e19d96374735c090073448092d30a
Zahra Shokri <zvahedi5@gmail.com>
2023-05-09 17:45:28 +0430

    Change footer's button in product card with css

--- src/components/shared/NewProduct.js
+++ src/components/shared/NewProduct.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import { shorten } from '../../functions/shorten';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContextProvider } from '../../context/CartContext';
+import { shorten, isInCart, quantityCounter } from '../../functions/allFUNs';
 import styles from '../../styles/NewProduct.module.css';
+import trashIcon from '../../images/icon/trash.svg';
 
 const NewProduct = ({ data }) => {
+    const {state, dispatch} = useContext(CartContextProvider);
+    
     return(
         <div className={styles.container}>
             <img src={data.image} alt='products' />
@@ -12,7 +16,21 @@ const NewProduct = ({ data }) => {
 
             <div className={styles.footerBtns}>
                 <Link to={ `/products/${data.id}` }>Details</Link>
-                <button> Add to Cart </button>
+
+                {
+                    (quantityCounter(state, data.id) === 1) && 
+                        <button className={styles.rmItemBtn} onClick={() => dispatch({type: 'REMOVE_ITEM', payload: data})}> <img src={trashIcon} alt='Trash Icon' /> </button>                    
+                }
+                {
+                    (quantityCounter(state, data.id) > 1) && 
+                        <button className={styles.decBtn} onClick={() => dispatch({type: 'DECREASE', payload: data})}> - </button>
+                }
+
+                {
+                    (isInCart(state, data.id))?
+                        <button className={styles.incBtn} onClick={() => dispatch({type: 'INCREASE', payload: data})}> + </button> : 
+                        <button className={styles.addItemBtn} onClick={() => dispatch({type: 'ADD_ITEM', payload: data})}> Add to Cart </button> 
+                }
             </div>
         </div>
     );
