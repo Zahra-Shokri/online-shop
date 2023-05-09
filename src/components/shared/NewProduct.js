6f51e741c4fe1eecf435e5c42b19706d68a92b72
Zahra Shokri <zvahedi5@gmail.com>
2023-05-09 22:28:59 +0330

    Change name of class for remove, increase & decrease buttons

--- src/components/shared/NewProduct.js
+++ src/components/shared/NewProduct.js
@@ -1,9 +1,19 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
+// Context
 import { CartContextProvider } from '../../context/CartContext';
+
+// Functions
 import { shorten, isInCart, quantityCounter } from '../../functions/allFUNs';
+
+// Styles
 import styles from '../../styles/NewProduct.module.css';
+import styles2 from '../../styles/common.module.css';
+
+// Icon
 import trashIcon from '../../images/icon/trash.svg';
+
 
 const NewProduct = ({ data }) => {
     const {state, dispatch} = useContext(CartContextProvider);
@@ -19,16 +29,16 @@ const NewProduct = ({ data }) => {
 
                 {
                     (quantityCounter(state, data.id) === 1) && 
-                        <button className={styles.rmItemBtn} onClick={() => dispatch({type: 'REMOVE_ITEM', payload: data})}> <img src={trashIcon} alt='Trash Icon' /> </button>                    
+                        <button className={styles2.trashIcn} onClick={() => dispatch({type: 'REMOVE_ITEM', payload: data})}> <img src={trashIcon} alt='Trash Icon' /> </button>                    
                 }
                 {
                     (quantityCounter(state, data.id) > 1) && 
-                        <button className={styles.decBtn} onClick={() => dispatch({type: 'DECREASE', payload: data})}> - </button>
+                        <button className={styles2.decBtn} onClick={() => dispatch({type: 'DECREASE', payload: data})}> - </button>
                 }
 
                 {
                     (isInCart(state, data.id))?
-                        <button className={styles.incBtn} onClick={() => dispatch({type: 'INCREASE', payload: data})}> + </button> : 
+                        <button className={styles2.incBtn} onClick={() => dispatch({type: 'INCREASE', payload: data})}> + </button> : 
                         <button className={styles.addItemBtn} onClick={() => dispatch({type: 'ADD_ITEM', payload: data})}> Add to Cart </button> 
                 }
             </div>
