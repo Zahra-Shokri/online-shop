93b1b673a5f220dc6cd758d3a8430bafaacb1f28
Zahra Shokri <zvahedi5@gmail.com>
2023-05-09 19:07:07 +0430

    Define sumItems Function to calculate products counter & total price

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,12 +1,16 @@
 import React, { createContext, useReducer } from 'react';
 
-export const CartContextProvider = createContext();
-
 const initialState = {
     selectedItems: [],
     itemsCounter: 0,
     total: 0,
     checkout: false
+}
+
+const sumItems = (items) => {
+    const itemsCounter = items.reduce((total, product) => total + product.quantity, 0);
+    const total = items.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2);
+    return { itemsCounter, total }
 }
 
 const cartReducer = (state, action) => {
@@ -18,21 +22,35 @@ const cartReducer = (state, action) => {
                     quantity: 1
                 });
             }
-            return { ...state, selectedItems: [...state.selectedItems] }
+            return { 
+                ...state, 
+                selectedItems: [...state.selectedItems], 
+                ...sumItems(state.selectedItems) 
+            }
 
         case 'REMOVE_ITEM': 
             const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id);
-            return { ...state, selectedItems: [...newSelectedItems] }
+            return { 
+                ...state, 
+                selectedItems: [...newSelectedItems],
+                ...sumItems(state.selectedItems)
+            }
 
         case 'INCREASE':
             const indexI = state.selectedItems.findIndex(item => item.id === action.payload.id)
             state.selectedItems[indexI].quantity++;
-            return { ...state }
+            return { 
+                ...state,
+                ...sumItems(state.selectedItems)
+            }
 
         case 'DECREASE':
             const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id)
             state.selectedItems[indexD].quantity--;
-            return { ...state }
+            return { 
+                ...state,
+                ...sumItems(state.selectedItems)
+            }
 
         case 'CHECKOUT':
             return {
@@ -53,6 +71,8 @@ const cartReducer = (state, action) => {
             return {state}
     }
 }
+
+export const CartContextProvider = createContext();
 
 const CartContext = (props) => {
     const [state, dispatch] = useReducer(cartReducer, initialState);
