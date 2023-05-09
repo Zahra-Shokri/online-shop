2096a0b00f33b6f2f16b34d68003af901cc264b2
Zahra Shokri <zvahedi5@gmail.com>
2023-05-09 20:46:08 +0430

    Shop Cart List Component is created to show all products that user added to shop cart

---
+++ src/components/ShopCartList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Style
+import styles from '../styles/ShopCartList.module.css';
+
+const ShopCartList = () => {
+    return(
+        <div className={styles.container}>
+            <h1> shop cart list </h1>
+        </div>
+    );
+}
+
+export default ShopCartList;
