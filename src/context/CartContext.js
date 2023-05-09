6b87e4442ed38b6bf56d5c1f42e850e4565f460d
Zahra Shokri <zvahedi5@gmail.com>
2023-05-09 14:20:23 +0430

    Cart Context Component is created to handle buttons in Products Page

---
+++ src/context/CartContext.js
@@ -0,0 +1,65 @@
+import React, { createContext, useReducer } from 'react';
+
+export const CartContextProvider = createContext();
+
+const initialState = {
+    selectedItems: [],
+    itemsCounter: 0,
+    total: 0,
+    checkout: false
+}
+
+const cartReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_ITEM': 
+            if(!state.selectedItems.find(item => item.id === action.payload.id)) {
+                state.selectedItems.push({
+                    ...action.payload,
+                    quantity: 1
+                });
+            }
+            return { ...state, selectedItems: [...state.selectedItems] }
+
+        case 'REMOVE_ITEM': 
+            const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id);
+            return { ...state, selectedItems: [...state.newSelectedItems] }
+
+        case 'INCREASE':
+            const indexI = state.selectedItems.findIndex(item => item.id === action.payload.id)
+            state.selectedItems[indexI].quantity++;
+            return { ...state }
+
+        case 'DECREASE':
+            const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id)
+            state.selectedItems[indexD].quantity--;
+            return { ...state }
+
+        case 'CHECKOUT':
+            return {
+                selectedItems: [],
+                itemsCounter: 0,
+                total: 0,
+                checkout: true
+            }
+
+        case 'CLEAR':
+            return {
+                selectedItems: [],
+                itemsCounter: 0,
+                total: 0,
+                checkout: false
+            }
+    }
+}
+
+const CartContext = () => {
+    const [state, dispatch] = useReducer(cartReducer, initialState);
+
+    return(
+        <CartContextProvider value={{state, dispatch}}>
+            { this.props.children }
+        </CartContextProvider>
+    );
+}
+
+export default CartContext;
