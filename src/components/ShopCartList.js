48c3ce5d6947a99b459d99399b0ea63b55780014
Zahra Shokri <zvahedi5@gmail.com>
2023-05-13 12:34:35 +0330

    Change Shop Cart List Component for show selected products & checkout panel with CSS

--- src/components/ShopCartList.js
+++ src/components/ShopCartList.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+// Context
+import { CartContextProvider } from '../context/CartContext';
+
+// Components
+import ShopCart from './shared/ShopCart';
 
 // Style
 import styles from '../styles/ShopCartList.module.css';
 
+
 const ShopCartList = () => {
+    const {state, dispatch} = useContext(CartContextProvider);
+    
     return(
         <div className={styles.container}>
-            <h1> shop cart list </h1>
+            <div className={styles.cartContainer}>
+                {
+                    state.selectedItems.map(product => <ShopCart key={product.id} data={product} />)
+                }
+            </div>
+            {
+                (state.itemsCounter > 0) &&
+                    <div className={styles.totalPanel}>
+                        <p> <span> Total Items: </span> {state.itemsCounter} </p>
+                        <p> <span> Total Payments:  </span> {state.total} $ </p> 
+
+                        <div className={styles.totalPanelBtns}>
+                            <button className={styles.clear} onClick={() => dispatch({type: 'CLEAR'})}> Clear </button>
+                            <button className={styles.checkout} onClick={() => dispatch({type: 'CHECKOUT'})}> Checkout </button>
+                        </div>
+                    </div>
+            }
+            {
+                (state.checkout) && 
+                    <div className={styles.compelete}>
+                        <h3>Checked Out Successfuly.</h3>
+                        <Link to='/products'> Buy more </Link>
+                    </div>
+            }
+            {
+                (!state.checkout && state.itemsCounter === 0) && 
+                    <div className={styles.compelete}>
+                        <h3>Do you want to buy?</h3>
+                        <Link to='/products'> Back to shop </Link>
+                    </div>
+            }
         </div>
     );
 }
