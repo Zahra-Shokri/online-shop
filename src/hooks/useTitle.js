c7447a8a268a6ccddd0e2af3dfaa4c30583fa725
Zahra Shokri <zvahedi5@gmail.com>
2023-05-22 18:36:34 +0330

    Bagfix a warning - add titleName in useEffect []

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const useTitle = (titleName) => {
     useEffect(() => {
         document.title = titleName;
-    }, [])
+    }, [titleName])
 }
 
 export default useTitle;
